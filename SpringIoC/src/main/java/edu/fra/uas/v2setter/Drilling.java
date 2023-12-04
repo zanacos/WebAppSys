d9c36a3816de28e983462729e0bc4321ad3e37d7
zanacos <zana.cosic@stud.fra-uas.de>
2023-12-04 20:42:09 +0100

    drilling

--- SpringIoC/src/main/java/edu/fra/uas/v2setter/Drilling.java
+++ SpringIoC/src/main/java/edu/fra/uas/v2setter/Drilling.java
@@ -6,6 +6,12 @@ import org.slf4j.LoggerFactory;
 public class Drilling implements Work {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Drilling.class);
+   
+
+    MasterV2 masterV2 = new MasterV2 ();
+    masterV2.setJourneymanAndWork(new Journeyman(), new Drilling());
+    
+    masterV2.delegateWork();
 
     public void doWork() {
         LOGGER.info(" --> drill a hole into the wall");
